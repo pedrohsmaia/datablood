3d4b718c6b00d944bac189b3c82b0b688e78c654
Alireza Zamani <alirezazamani2922@gmail.com>
2023-07-24 13:34:20 +0330

    fix: react-hook-form breaking some pages on SSR

--- packages/ui/src/components/SubmitButton.tsx
+++ packages/ui/src/components/SubmitButton.tsx
@@ -1,36 +1,42 @@
 import { useFormState } from 'react-hook-form'
 import { AnimatePresence, Button, ButtonProps, Spinner } from 'tamagui'
 
+// hack to prevent it from breaking on the server
+const useIsSubmitting = () => {
+  try {
+    return useFormState().isSubmitting
+  } catch (error) {
+    return false
+  }
+}
 /**
  * created to be used in forms
  * will show loading spinners and disable submission when already submitting
  */
 export const SubmitButton = (props: ButtonProps) => {
-  const { isSubmitting } = useFormState()
+  const isSubmitting = useIsSubmitting()
   return (
     <Button
       iconAfter={
-        isSubmitting ? (
-          <AnimatePresence>
-            {isSubmitting && (
-              <Spinner
-                color="$color"
-                key="loading-spinner"
-                opacity={1}
-                y={0}
-                animation="quick"
-                enterStyle={{
-                  opacity: 0,
-                  y: 4,
-                }}
-                exitStyle={{
-                  opacity: 0,
-                  y: 4,
-                }}
-              />
-            )}
-          </AnimatePresence>
-        ) : undefined
+        <AnimatePresence>
+          {isSubmitting && (
+            <Spinner
+              color="$color"
+              key="loading-spinner"
+              opacity={1}
+              y={0}
+              animation="quick"
+              enterStyle={{
+                opacity: 0,
+                y: 4,
+              }}
+              exitStyle={{
+                opacity: 0,
+                y: 4,
+              }}
+            />
+          )}
+        </AnimatePresence>
       }
       disabled={isSubmitting}
       {...props}
