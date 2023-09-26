118828e4cf4b061e6a438de640883a55e3e0705f
benjamin <benschac@gmail.com>
2023-09-26 11:00:03 -0400

    add eslint overrides

--- apps/next/.eslintrc.js
+++ apps/next/.eslintrc.js
@@ -1,4 +1,12 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   extends: ['next', 'custom'],
+  overrides: [
+    {
+      files: ['next.config.js'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
+  ],
 }
