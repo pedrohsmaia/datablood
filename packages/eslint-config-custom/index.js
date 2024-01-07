0c31d70eaeb29748b0c1fc288e45a9f49d1faf0a
Alireza Zamani <alirezazamani2922@gmail.com>
2024-01-07 06:51:19 +0330

    fix: ESLint and TypeScript setup regressions + simplify the config and remove unnecessary deps

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,10 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
+    'universe/native',
   ],
   overrides: [
     {
