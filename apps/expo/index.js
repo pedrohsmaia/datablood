1fd58ade705f05665d243274bb27681b05266ce3
ROFI <rofi@rodrigos-mbp.lan>
2024-05-22 15:51:40 -0400

    fix: AnimatedComponent build error

--- apps/expo/index.js
+++ apps/expo/index.js
@@ -1,6 +1,9 @@
 import { registerRootComponent } from 'expo'
 import { ExpoRoot } from 'expo-router'
 import 'react-native-url-polyfill/auto'
+import React from 'react'
+
+React.AnimatedComponent = ({ children }) => <>{children}</>
 
 // Must be exported or Fast Refresh won't update the context
 export function App() {
