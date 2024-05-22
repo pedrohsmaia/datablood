fb81031285a8cb2449191d47fbb917981b46478b
ROFI <rofi@rodrigos-mbp.lan>
2024-05-22 17:07:57 -0400

    add note on index.js workaround for build to work w/ expo v51

--- apps/expo/index.js
+++ apps/expo/index.js
@@ -3,6 +3,7 @@ import { ExpoRoot } from 'expo-router'
 import 'react-native-url-polyfill/auto'
 import React from 'react'
 
+//NOTE: do not remove. this is a workaround for build to work with expo v51.0.0
 React.AnimatedComponent = ({ children }) => <>{children}</>
 
 // Must be exported or Fast Refresh won't update the context
