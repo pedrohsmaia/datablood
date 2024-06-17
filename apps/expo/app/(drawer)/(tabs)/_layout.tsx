792b3d65762e610a4ec9757950279c3616f3ee5a
ROFI <rofi@Rodrigos-MacBook-Pro.local>
2024-06-17 14:18:25 -0400

    expo router setup to handle drawer and modals

---
+++ apps/expo/app/(drawer)/(tabs)/_layout.tsx
@@ -0,0 +1,95 @@
+import { Avatar, Circle, type ColorTokens, Theme, YStack, validToken, useTheme } from '@my/ui'
+import type { BottomTabNavigationOptions } from '@react-navigation/bottom-tabs'
+import { LinearGradient } from '@tamagui/linear-gradient'
+import { Home, Plus, Settings } from '@tamagui/lucide-icons'
+import { useUser } from 'app/utils/useUser'
+import { Stack, Tabs, router, useNavigation } from 'expo-router'
+import { Drawer } from 'expo-router/drawer'
+import { Pressable } from 'react-native'
+import { SolitoImage } from 'solito/image'
+import { useRouter } from 'solito/router'
+import { Menu } from '@tamagui/lucide-icons'
+
+export default function Layout() {
+  const { accentColor } = useTheme()
+  const navigation = useNavigation()
+  return (
+    <>
+      <Stack.Screen
+        options={{
+          title: 'Home',
+          headerTintColor: accentColor.val,
+          headerLeft: ({}) => (
+            <Pressable
+              marginLeft={12}
+              onPress={() => {
+                navigation.openDrawer()
+              }}
+            >
+              <Menu size={24} />
+            </Pressable>
+          ),
+          headerRight: ({}) => (
+            <Pressable
+              marginRight={12}
+              onPress={() => {
+                navigation.navigate('create')
+              }}
+            >
+              <Plus size={24} />
+            </Pressable>
+          ),
+        }}
+      />
+      <Tabs
+        screenOptions={{
+          tabBarShowLabel: false,
+          headerTintColor: accentColor.val,
+          tabBarActiveTintColor: accentColor.val,
+          tabBarInactiveTintColor: accentColor.val,
+        }}
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: 'Home test',
+            tabBarIcon: ({ size, color, focused }) => (
+              <Home col={focused ? '$gray10' : (color as ColorTokens)} size={size} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="settings"
+          options={{
+            title: 'Settings',
+            tabBarIcon: ({ size, color, focused }) => (
+              <Settings col={focused ? '$gray10' : (color as ColorTokens)} size={size} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: 'Profile',
+            tabBarIcon: ({ size, color, focused }) => (
+              <Settings col={focused ? '$gray10' : (color as ColorTokens)} size={size} />
+            ),
+          }}
+        />
+      </Tabs>
+    </>
+  )
+}
+
+type TabBarIconProps = Parameters<Exclude<BottomTabNavigationOptions['tabBarIcon'], undefined>>[0]
+
+const ProfileTabIcon = ({ color, size }: TabBarIconProps) => {
+  const { avatarUrl } = useUser()
+  return (
+    <YStack bw="$1" boc={validToken(color)} br="$10">
+      <Avatar circular p="$1" size={size}>
+        <SolitoImage src={avatarUrl} alt="your avatar" width={size} height={size} />
+      </Avatar>
+    </YStack>
+  )
+}
